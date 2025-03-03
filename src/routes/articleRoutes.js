22a7a1ccda04a4b2eb40cdcae9ce01c2cf0e1592
unknown <Tuantumat203@gmail.com>
2025-03-04 00:20:45 +0700

    Tách thành controller, routes +  bổ sung console log (URL, result)

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -1,72 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../config/db");
+const articleController = require("../controllers/articleController");
 
 // Truy vấn tất cả articles
-router.get("/", async (req, res) => {
-    try {
-      const result = await pool.query("SELECT * FROM articles");
-      console.log(originalUrl);
-      res.json(result.rows);
-    } catch (err) {
-      console.error("Lỗi truy vấn:", err);
-      res.status(500).send("Lỗi server");
-    }
-  });
+router.get("/", articleController.getArticles);
 
-  //Thêm một article mới
-  router.post("/new", async (req, res) => {
-    try {
-      const { title, content } = req.body;
-      const result = await pool.query(  
-        "INSERT INTO articles (title, content, status) VALUES ($1, $2, 'completed') RETURNING *",
-        [title, content]
-      );
-      console.log(originalUrl);
-      res.json(result.rows[0]);
-    } catch (err) {
-      console.error("Lỗi truy vấn:", err);
-      res.status(500).send("Lỗi server");
-    }
-  });
+//Thêm một article mới
+  router.post("/new", articleController.addArticle);
 
   //Xoá một article
-  router.delete("/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const result = await pool.query("DELETE FROM articles WHERE id = $1 RETURNING *", [id]);
-      console.log(originalUrl);
-      if (result.rowCount === 0)  
-        return res.status(404).send("Article not found");
-      else
-        res.json({ message: "Article deleted successfully" , deletedArticle: result.rows[0] });
-    } catch (err) {
-      console.error("Lỗi truy vấn:", err);
-      res.status(500).send("Lỗi server");
-    }
-  });
+  router.delete("/:id", articleController.deleteArticle);
 
   //Chỉnh sửa một article
-  router.put("/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { title, content } = req.body;
-      const timestamp = new Date(Date.now()).toISOString();
-      const result = await pool.query(
-        "UPDATE articles SET title = $1, content = $2, updated_at = $3 WHERE id = $4 RETURNING *",
-        [title, content, timestamp, id]
-      );
-      console.log(originalUrl);
-      if (result.rowCount === 0)  
-        return res.status(404).send("Article not found");
-      else
-        res.json({ message: "Article updated successfully", updatedArticle: result.rows[0] });
-    } catch (err) {
-      console.error("Lỗi truy vấn:", err);
-      res.status(500).send("Lỗi server");
-    }
-  });
-
-  
+  router.put("/:id", articleController.updateArticle);
 
   module.exports = router
