6ed898a770e2c5b48c4ef6f84ad0bb68e8d93f34
gnourt0307 <truongtkls8@gmail.com>
2025-03-03 10:07:55 +0700

    change route folder

--- src/controllers/demo-controller.js
+++ src/controllers/demo-controller.js
@@ -1,16 +1,16 @@
-const createError = require("http-errors")
+const createError = require("http-errors");
 
 const demoGetController = (req, res) => {
-	res.json({
-		message: "Hello, World!",
-	})
-}
+  res.json({
+    message: "Hello, World!",
+  });
+};
 
 const demoErrorController = (req, res, next) => {
-	next(createError(400, "This is a demo error"))
-}
+  next(createError(400, "This is a demo error"));
+};
 
 module.exports = {
-	demoGetController,
-	demoErrorController,
-}
+  demoGetController,
+  demoErrorController,
+};
