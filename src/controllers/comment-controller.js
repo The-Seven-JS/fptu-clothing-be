01ae8a2cee966fc9a974536be13525491a2a8d0c
unknown <Tuantumat203@gmail.com>
2025-03-22 11:02:55 +0700

    Thêm 4 function cơ bản của Comment Controller

---
+++ src/controllers/comment-controller.js
@@ -0,0 +1,96 @@
+const pool = require("../config/db");
+
+const getComments = async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM comments");
+        console.log (result.rows);
+        if (result.rows.length === 0) {
+            return res.status(404).send("Comments not found");
+        }
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error("Lỗi truy vấn:", err);
+        res.status(500).send("Lỗi server");
+    }
+};
+
+const getComment = async (req, res) => {
+    try {
+        const { article_id } = req.params;
+        const result = await pool.query("SELECT * FROM comments WHERE article_id = $1", [article_id]);
+        if (result.rows.length === 0) {
+            return res.status(404).send("Comment not found");
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        console.error("Lỗi truy vấn:", err);
+        res.status(500).send("Lỗi server");
+    }
+};
+
+const addComment = async (req, res) => {
+    try {
+        const article_id = req.params.article_id;
+        const result_article_id = await pool.query("SELECT * FROM articles WHERE id = $1", [article_id]);
+        console.log ("RESULT ARTICLE ID: " , result_article_id);
+        if (result_article_id.rows.length === 0) {
+            return res.status(404).send("Article not found");
+        }
+        const username = req.body.username;
+        console.log ("USERNAME: " , username);
+        const content = req.body.content;
+        console.log ("CONTENT: " , content);
+        if (typeof content !== "string" || typeof username !== "string") {
+            return res.status(400).send("username and content must be strings");
+        }
+        if (username === undefined || content === undefined) {
+            return res.status(400).send("username and content must be defined");
+        }
+
+         if (username.trim() === "<p></p>" || content.trim() === "<p></p>") {
+            return res.status(400).send("Usrname and content should not be empty");
+        }
+        const email = req.body.email;
+        console.log ("EMAIL: " , email);
+        if (typeof email !== "string") {
+            return res.status(400).send("email must be a string");
+        }
+        if (email === undefined) {
+            return res.status(400).send("email must be defined");
+        }
+        const result = await pool.query("INSERT INTO comments (username, content, email, article_id) VALUES ($1, $2, $3, $4) RETURNING *", [username, content, email,article_id]);
+        console.log(result.rows);
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error("Lỗi truy vấn:", err);
+        res.status(500).send("Lỗi server");
+    }
+};
+
+const deleteComment = async (req, res) => {
+    try {
+        const { comment_id } = req.params;
+        const result_comment_id = await pool.query("SELECT * FROM comments WHERE id = $1", [comment_id]);
+        if (result_comment_id.rows.length === 0) {
+            return res.status(404).send("Comment not found");
+        }
+        const article_id = req.params.article_id;
+        const result_article_id = await pool.query("SELECT * FROM articles WHERE id = $1", [article_id]);
+        if (result_article_id.rows.length === 0) {
+            return res.status(404).send("Article not found");
+        }
+        if (result_comment_id.rows[0].article_id !== article_id) {
+            return res.status(404).send("Conflict between comment_id and article_id");
+        }
+        const result = await pool.query("DELETE FROM comments WHERE comment_id = $1 RETURNING *", [comment_id]);
+        if (result.rows.length === 0) {
+            return res.status(404).send("Comment not found");
+        }
+        res.status(200).json({ message: "Comment deleted successfully" , comment: result.rows[0]});
+    } catch (err) {
+        console.error("Lỗi truy vấn:", err);
+        res.status(500).send("Lỗi server");
+    }
+};
+
+module.exports = { getComments, getComment, addComment, deleteComment };
--- src/routes/comment-route.js
+++ src/routes/comment-route.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/comment-controller");
 
+// Lấy toàn bộ comments
+router.get("/", commentController.getComments);
+// Lấy 1 comment theo id bài viết
+router.get('/:article_id', commentController.getComment);
+// Thêm 1 comment mới vào bài viết cụ thể
+router.post("/new/:article_id", commentController.addComment);
+// Xoá 1 comment của 1 bài viết cụ thể
+router.delete("/delete/:article_id/:comment_id", commentController.deleteComment);
 
 
 
