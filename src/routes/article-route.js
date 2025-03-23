64cddc1b98a60f4c085e805003a291de9490fe9e
gnourt0307 <truongtkls8@gmail.com>
2025-03-24 00:17:23 +0700

    add require authorization

--- src/routes/article-route.js
+++ src/routes/article-route.js
@@ -4,28 +4,25 @@ const articleController = require("../controllers/article-controller");
 const requireAuth = require("../middleware/authmiddleware");
 
 // Truy vấn tất cả articles
-  router.get("/", articleController.getArticles);
+router.get("/", articleController.getArticles);
 
 //Truy vấn 1 article
-  router.get("/:article_id", articleController.getArticle);
+router.get("/:article_id", articleController.getArticle);
 
 //Thêm một article mới
 router.post("/new", requireAuth, articleController.addArticle);
 
 //Xoá một article
-<<<<<<< HEAD
-router.delete("/:article_id", requireAuth, articleController.deleteArticle);
+router.delete(
+  "/delete-article/:article_id",
+  requireAuth,
+  articleController.deleteArticle
+);
+
+// Xoá một article draft
+router.delete("/delete-draft", requireAuth, articleController.deleteDraft);
 
 //Chỉnh sửa một article
 router.put("/:article_id", requireAuth, articleController.updateArticle);
-=======
-  router.delete("/delete-article/:article_id", articleController.deleteArticle);
-
-// Xoá một article draft
-  router.delete("/delete-draft", articleController.deleteDraft);
-  
-//Chỉnh sửa một article
-  router.put("/:article_id", articleController.updateArticle);
->>>>>>> 099670ad3da23f451b668bb6684f340ddcb07a23
 
 module.exports = router;
