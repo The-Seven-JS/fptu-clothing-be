498ef186035f291096f7cf148b86920dd7ae13cf
gnourt0307 <truongtkls8@gmail.com>
2025-03-25 20:09:29 +0700

    fix cors

--- src/index.js
+++ src/index.js
@@ -24,6 +24,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://app.fuct.gay",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
     credentials: true,
   })
 );
